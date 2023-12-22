a535617997535619ff8a1ec925ca97571f897e55
Vincenzo D'Aniello <vidaniello@gmail.com>
2023-12-22 17:07:29 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+
+
+const element = document.createElement('div');
+element.innerHTML = 'Hi';
+
+
+document.addEventListener('DOMContentLoaded', (f)=>{
+    document.body.appendChild(element);
+});
---
+++ test/test_dist/main.js
@@ -0,0 +1,14 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+var element = document.createElement('div');
+element.innerHTML = 'Hi';
+document.addEventListener('DOMContentLoaded', function (f) {
+    document.body.appendChild(element);
+});
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EsQ0FBQyIsInNvdXJjZXMiOlsid2VicGFjazovL0B2aWRhbmllbGxvL3JlbW90ZS1jb250cm9sLWNsaWVudC8uL3NyYy9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJ2YXIgZWxlbWVudCA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoJ2RpdicpO1xuZWxlbWVudC5pbm5lckhUTUwgPSAnSGknO1xuZG9jdW1lbnQuYWRkRXZlbnRMaXN0ZW5lcignRE9NQ29udGVudExvYWRlZCcsIGZ1bmN0aW9uIChmKSB7XG4gICAgZG9jdW1lbnQuYm9keS5hcHBlbmRDaGlsZChlbGVtZW50KTtcbn0pO1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
