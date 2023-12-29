27f358f5de3077f589d79bf47f5ac37db86ac1ab
Vincenzo D'Aniello <vidaniello@gmail.com>
2023-12-29 17:25:31 +0100

    Progress develop

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -25,4 +27,15 @@ module.exports = {
       static: './test',
       https: true
      },
+     plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Output Management',
+      }),
+      new WorkboxPlugin.GenerateSW({
+        // these options encourage the ServiceWorkers to get in there fast
+        // and not allow any straggling "old" SWs to hang around
+        clientsClaim: true,
+        skipWaiting: true,
+      }),
+     ]
   };
