08e53cc47b0f6591e6531f1c37150937916fe5ee
vidaniello <vidaniello@gmail.com>
2024-01-02 07:32:11 +0100

    develop progress

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+//import * as pack from '../package.json';
 
 
 const element = document.createElement('div');
@@ -7,3 +8,13 @@ element.innerHTML = 'Hi';
 document.addEventListener('DOMContentLoaded', (f)=>{
     document.body.appendChild(element);
 });
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js').then(registration => {
+        console.log('SW registered: ', registration);
+      }).catch(registrationError => {
+        console.log('SW registration failed: ', registrationError);
+      });
+    });
+  }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+
 
 module.exports = {
     mode: 'development',
@@ -20,7 +22,7 @@ module.exports = {
     },
     output: {
       filename: 'main.js',
-      path: path.resolve(__dirname, 'test/test_dist'),
+      path: path.resolve(__dirname, 'test/'),
       clean: true
     },
     devServer: {
@@ -29,7 +31,10 @@ module.exports = {
      },
      plugins: [
       new HtmlWebpackPlugin({
-        title: 'Output Management',
+        title: 'RemoteControl',
+        template: 'src/template.index.html',
+        favicon: 'assets/favicon.ico',
+        
       }),
       new WorkboxPlugin.GenerateSW({
         // these options encourage the ServiceWorkers to get in there fast
@@ -37,5 +42,27 @@ module.exports = {
         clientsClaim: true,
         skipWaiting: true,
       }),
+      new WebpackManifestPlugin({
+        seed: {
+          version: "1.0.0",
+          name: 'RemoteControlClient',
+          short_name: "Super",
+          theme_color: "#ffffff",
+          background_color: "#ffffff",
+          display: "standalone",
+          icons: [
+            {
+                src: "/android-chrome-192x192.png",
+                sizes: "192x192",
+                type: "image/png"
+            },
+            {
+                src: "/android-chrome-512x512.png",
+                sizes: "512x512",
+                type: "image/png"
+            }
+        ],
+        }
+      })
      ]
   };
