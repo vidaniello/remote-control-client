a008594e695e3ead9d6b3c33160cb0cbfad4fb8c
Vincenzo D'Aniello <vidaniello@gmail.com>
2024-01-02 17:34:12 +0100

    develop progress

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 //import * as pack from '../package.json';
-
+/*
+import favicon_16x316 from '../assets/favicon-16x16.png';
+import favicon_32x32 from '../assets/favicon-32x32.png';
+import apple_touch_icon from '../assets/apple-touch-icon.png';
+import android_chrome_192x192 from '../assets/android-chrome-192x192.png';
+import android_chrome_512x512 from '../assets/android-chrome-512x512.png';
+*/
 
 const element = document.createElement('div');
-element.innerHTML = 'Hi';
-
+element.innerHTML = 'Hi world';
 
 document.addEventListener('DOMContentLoaded', (f)=>{
     document.body.appendChild(element);
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
-
+const CopyPlugin = require("copy-webpack-plugin");
+const manifest = require("./assets/site.webmanifest.json");
 
 module.exports = {
     mode: 'development',
@@ -14,7 +15,13 @@ module.exports = {
           test: /\.tsx?$/,
           use: 'ts-loader',
           exclude: /node_modules/,
+        },
+        /*
+        {
+          test: /\.(png|jpg|jpeg|gif)$/i,
+          type: 'asset/resource'
         }
+        */
       ]
     },
     resolve: {
@@ -30,39 +37,62 @@ module.exports = {
       https: true
      },
      plugins: [
+
       new HtmlWebpackPlugin({
         title: 'RemoteControl',
         template: 'src/template.index.html',
         favicon: 'assets/favicon.ico',
-        
+        manifest: 'site.webmanifest',
+        meta: {
+          appleTouchIcon: {
+            name: 'apple-touch-icon',
+            content: 'apple-touch-icon.png'
+          },
+          appleMobileWebAppC: {
+            name: 'apple-mobile-web-app-capable',
+            content: 'yes'
+          },
+          appleMobileWebAppSBS: {
+            name: 'apple-mobile-web-app-status-bar-style',
+            content: 'black'
+          },
+          appleMobileWebAppT: {
+            name: 'apple-mobile-web-app-title',
+            content: manifest.name
+          },
+          description: {
+            name: 'description',
+            content: 'Remote client'
+          },
+          themeColor: {
+            name: 'theme-color',
+            content: manifest.theme_color
+          }
+        },
       }),
+
       new WorkboxPlugin.GenerateSW({
         // these options encourage the ServiceWorkers to get in there fast
         // and not allow any straggling "old" SWs to hang around
         clientsClaim: true,
         skipWaiting: true,
+        additionalManifestEntries: [
+          {url: 'site.webmanifest', revision: null}
+        ]
       }),
+      
       new WebpackManifestPlugin({
-        seed: {
-          version: "1.0.0",
-          name: 'RemoteControlClient',
-          short_name: "Super",
-          theme_color: "#ffffff",
-          background_color: "#ffffff",
-          display: "standalone",
-          icons: [
-            {
-                src: "/android-chrome-192x192.png",
-                sizes: "192x192",
-                type: "image/png"
-            },
-            {
-                src: "/android-chrome-512x512.png",
-                sizes: "512x512",
-                type: "image/png"
-            }
-        ],
-        }
+        fileName: 'site.webmanifest',
+        publicPath: '',
+        seed: manifest
       })
+
+      ,
+      new CopyPlugin({
+        patterns: [
+          { from: "assets/*.png", to: "[name][ext]" }
+        ],
+      })
+
      ]
   };
